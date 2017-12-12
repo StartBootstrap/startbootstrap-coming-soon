0be0446da95649f14dc752f071a43d0789e761d6
David Miller <hello@davidmiller.io>
2017-12-12 17:33:49 -0500

    initial commit

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/bg.mp4",
+    poster: "img/bg-mobile-fallback.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
