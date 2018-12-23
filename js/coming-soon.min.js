1fce2d54b06fc4dad3eb8aadfd56cbbc3f0b4ebd
David Miller <hello@davidmiller.io>
2018-12-22 22:13:38 -0700

    gulp 4 update and dependencies update
    bootstrap 4.2.1 and font awesome update

--- js/coming-soon.min.js
+++ js/coming-soon.min.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Coming Soon v5.0.0 (https://startbootstrap.com/template-overviews/coming-soon)
+ * Start Bootstrap - Coming Soon v5.0.1 (https://startbootstrap.com/template-overviews/coming-soon)
  * Copyright 2013-2018 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-coming-soon/blob/master/LICENSE)
  */
